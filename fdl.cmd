33eb6569f2c3537e3ebf17e785053923112f4cc6
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-08-18 21:12:58 +0200

    Support legacy curl versions

--- fdl.cmd
+++ fdl.cmd
@@ -24,13 +24,9 @@ if exist "%~dp0curl.exe" set CURL="%~dp0curl"
 :: 1 arg: download into cwd, auto-detect file name
 :: 2 args: download into chosen dir, auto-detect file name
 :: 3 args: download into chosen dir, use given file name
-if "%~2" == "" (
-  %CURL% -sfkLO %1
-) else if "%~3" == "" (
-  %CURL% --output-dir %2 -sfkLO %1
-) else (
-  %CURL% --output-dir %2 -sfkLo %3 %1
-)
+if not "%~2" == "" pushd %2
+if not "%~3" == "" (%CURL% -sfkLo %3 %1) else (%CURL% -sfkLO %1)
+if not "%~2" == "" popd
 
 :: Log download errors into STDERR
 if errorlevel 1 echo %DATE% %TIME:~0,-3% [%ERRORLEVEL%] %1 >&2
